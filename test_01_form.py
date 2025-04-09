efc154d69f9e7fd5f8ed2c45953efb956931712e
alanSakaev <smr.alan@yandex.ru>
2025-04-09 11:30:58 +0300

    Добавлен файл с зависимостями, добавлена автоматизация поиска ChromeDriver через webdriver-manager

--- test_01_form.py
+++ test_01_form.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,7 +8,7 @@ from selenium.common.exceptions import TimeoutException
 from time import sleep
 import pytest
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 def filling_in_fields(
     first_name_param="",
